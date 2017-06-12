ff5815466c74851e94650faa803ea1b240637298
tackeyy <mailto.takita.yusuke@gmail.com>
2017-06-12 15:05:51 +0900

    Add .tigrc

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DOTFILES=(.vim .vimrc .dein.toml .dein_lazy.toml .tmux .tmux.conf .zshrc .zprofile .agignore .zprofile .bashrc .bash_profile .git .gitconfig .config/peco/config.json)
+DOTFILES=(.vim .vimrc .dein.toml .dein_lazy.toml .tmux .tmux.conf .zshrc .zprofile .agignore .zprofile .bashrc .bash_profile .git .gitconfig .tigrc .config/peco/config.json)
 
 for file in ${DOTFILES[@]}
 do
